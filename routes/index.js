d0c2c5c9d5dbda9a668f7a4faec5b30b2a5833bd
XIAOJINHONG <1870232639@qq.com>
2017-07-15 20:41:07 +0800

    Add files from xiao
    Add files from xiao

--- routes/index.js
+++ routes/index.js
@@ -11,11 +11,117 @@ var router = express.Router();
 
 /* GET home page. */
 
+router.get("/", function(req, res, next) {//主页
+  questions.find({}).sort({"ltime":-1}).skip(0).limit(20).exec(function(err,data_que){
 
-router.get(/\/([0-9]{1,2})?/, function(req, res, next) {
+    if(err){
+      console.log(err);
+    }else{
+      users.find({}).sort({'likes':-1}).limit(10).exec(function(err,datas){
+    if(err){
+      console.log(err);
+    }else{
+      users.findOne({
+        nick:req.session.name // ======== ? 登录状态下的用户名
+      },function(err,data){
+        // console.log(data);
+        if(typeof(req.session.name) == 'undefined'){
+          req.session.name = null;
+        }
+
+      res.render('index',{ active0:"active",active1:"",active2:"",active3:"",page:"a",data_que:data_que, data_use:datas, data:data, name:req.session.name})
+
+    });
+  }
+});
+}
+});
+});
+router.get("/index_hot", function(req, res, next) {//热门回答
+  questions.find({}).sort({'answers':-1}).skip(0).limit(20).exec(function(err,data_que){
+
+    if(err){
+      console.log(err);
+    }else{
+      users.find({}).sort({'likes':-1}).limit(10).exec(function(err,datas){
+    if(err){
+      console.log(err);
+    }else{
+      users.findOne({
+        nick:req.session.name // ======== ? 登录状态下的用户名
+      },function(err,data){
+        // console.log(data);
+        if(typeof(req.session.name) == 'undefined'){
+          req.session.name = null;
+        }
+
+      res.render('index',{ active0:"",active1:"active",active2:"",active3:"",page:"b",data_que:data_que, data_use:datas, data:data, name:req.session.name})
+
+    });
+  }
+});
+}
+});
+});
+router.get("/index_wait", function(req, res, next) {
+  questions.find({"answers":0}).skip(0).limit(20).exec(function(err,data_que){
+
+    if(err){
+      console.log(err);
+    }else{
+      users.find({}).sort({'likes':-1}).limit(10).exec(function(err,datas){
+    if(err){
+      console.log(err);
+    }else{
+      users.findOne({
+        nick:req.session.name // ======== ? 登录状态下的用户名
+      },function(err,data){
+        // console.log(data);
+        if(typeof(req.session.name) == 'undefined'){
+          req.session.name = null;
+        }
+
+      res.render('index',{ active0:"",active1:"",active2:"active",active3:"",page:"c",data_que:data_que, data_use:datas, data:data, name:req.session.name})
+
+    });
+  }
+});
+}
+});
+});
+router.get("/#", function(req, res, next) {//最新回答
+  questions.find({}).skip(0).limit(20).exec(function(err,data_que){
+
+    if(err){
+      console.log(err);
+    }else{
+      users.find({}).sort({'likes':-1}).limit(10).exec(function(err,datas){
+    if(err){
+      console.log(err);
+    }else{
+      users.findOne({
+        nick:req.session.name // ======== ? 登录状态下的用户名
+      },function(err,data){
+        // console.log(data);
+        if(typeof(req.session.name) == 'undefined'){
+          req.session.name = null;
+        }
+
+      res.render('index',{ active0:"active",active1:"",active2:"",active3:"",page:"a",data_que:data_que, data_use:datas, data:data, name:req.session.name})
+
+    });
+  }
+});
+}
+});
+});
+
+router.get(/\/a([0-9]{1,2})/, function(req, res, next) {
+  console.log("------------------------------");
   // res.render('index', { title: 'Express' });
   var k = 0;
-  if (req.params[0]==00) {
+  if (req.params[0]==98) {
+    console.log("lingling");
     k++;
   }else if(req.params[0] == 99) {
     k--;
@@ -32,12 +138,12 @@ router.get(/\/([0-9]{1,2})?/, function(req, res, next) {
           console.log("-------",k);
        if(k < 0){
         k = 0;
-      }else if(k > count){
+      }else if(k >= count){
         k = 0;
         }
       }
 
-      questions.find({}).skip(k*20).limit(20).exec(function(err,data_que){
+      questions.find({}).sort({'ltime':-1}).skip(k*20).limit(20).exec(function(err,data_que){
 
         if(err){
           console.log(err);
@@ -55,13 +161,119 @@ router.get(/\/([0-9]{1,2})?/, function(req, res, next) {
               req.session.name = null;
             }
 
-          res.render('index',{data_que:data_que, data_use:datas, data:data, name:req.session.name})
+          res.render('index',{ active0:"active",active1:"",active2:"",active3:"",page:"a",data_que:data_que, data_use:datas, data:data, name:req.session.name})
 
-        }
-      });
+        });
         }
 
       });
+    }
+  });
+
+});
+});
+router.get(/\/b([0-9]{1,2})/, function(req, res, next) {
+  var k = 0;
+  if (req.params[0]==98) {
+    k++;
+  }else if(req.params[0] == 99) {
+    k--;
+  }else{
+    k = req.params[0];
+  }
+   var count;
+    questions.count({},function(err,n){
+      if(err){
+        console.log(err);
+      }else{
+        count = Math.ceil(n/20) ;
+        console.log(count);
+          console.log("-------",k);
+       if(k < 0){
+        k = 0;
+      }else if(k >= count){
+        k = 0;
+        }
+      }
+
+      questions.find({}).sort({'answers':-1}).skip(k*20).limit(20).exec(function(err,data_que){
+
+        if(err){
+          console.log(err);
+        }else{
+          users.find({}).sort({'likes':-1}).limit(10).exec(function(err,datas){
+        if(err){
+          console.log(err);
+
+        }else{
+          users.findOne({
+            nick:req.session.name // ======== ? 登录状态下的用户名
+          },function(err,data){
+            // console.log(data);
+            if(typeof(req.session.name) == 'undefined'){
+              req.session.name = null;
+            }
+
+          res.render('index',{active0:"",active1:"active",active2:"",active3:"",page:"b",data_que:data_que, data_use:datas, data:data, name:req.session.name})
+
+        });
+        }
+
+      });
+    }
+  });
+
+});
+});
+router.get(/\/c([0-9]{1,2})/, function(req, res, next) {
+  var k = 0;
+  if (req.params[0]==98) {
+    k++;
+  }else if(req.params[0] == 99) {
+    k--;
+  }else{
+    k = req.params[0];
+  }
+   var count;
+    questions.count({},function(err,n){
+      if(err){
+        console.log(err);
+      }else{
+        count = Math.ceil(n/20) ;
+        console.log(count);
+          console.log("-------",k);
+       if(k < 0){
+        k = 0;
+      }else if(k >= count){
+        k = 0;
+        }
+      }
+
+      questions.find({"answers":0}).skip(k*20).limit(20).exec(function(err,data_que){
+
+        if(err){
+          console.log(err);
+        }else{
+          users.find({}).sort({'likes':-1}).limit(10).exec(function(err,datas){
+        if(err){
+          console.log(err);
+
+        }else{
+          users.findOne({
+            nick:req.session.name // ======== ? 登录状态下的用户名
+          },function(err,data){
+            // console.log(data);
+            if(typeof(req.session.name) == 'undefined'){
+              req.session.name = null;
+            }
+            res.render('index',{active0:"",active1:"",active2:"active",active3:"",page:"c",data_que:data_que, data_use:datas, data:data, name:req.session.name})
+
+
+        });
+        }
+
+      });
+    }
   });
 
 });

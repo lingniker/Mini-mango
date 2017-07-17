bab9ece9e1a5ccf37d006c83748cece6d9a5711d
lingker <lingyuanping@outbook.com>
2017-07-17 13:50:54 +0800

    fix bug
    a lot of bug

--- routes/users.js
+++ routes/users.js
@@ -8,23 +8,44 @@ router.get("/", function(req, res, next) {
   var str = req.query.user;
   console.log(str);
   console.log(req.query.info);
-  if(true){
+  if(req.query.user === req.session.name){
     if(req.query.info === "info"){
-      users.findOne({"nick":str},["info","nick","focuselist"],function(err,mydata){   //查找一个
-        res.render("info",{mydata:mydata,page:"my",info:"info"});
+      users.findOne({"nick":str},function(err,top_data){   //查找一个
+        res.render("info",{top_data:top_data,page:"my",info:"info",name:req.session.name});
       });
     }else  if(req.query.info === "focuslist"){
-    users.findOne({"nick":str},["info","nick","focuslist"],function(err,mydata){   //查找一个
-      res.render("info",{mydata:mydata,page:"my",info:"focuslist"});
+    users.findOne({"nick":str},function(err,top_data){   //查找一个
+      res.render("info",{top_data:top_data,page:"my",info:"focuslist",name:req.session.name});
     });
   }else if(req.query.info === "puber"){
-    users.findOne({"nick":str},["info","nick","focuslist"],function(err,mydata){
+    users.findOne({"nick":str},function(err,top_data){
     questions.find({puber:[req.query.user]},function(err,qdata){
-      res.render("info",{mydata:mydata,page:"my",info:"puber"});
+      console.log(qdata);
+      res.render("info",{top_data:top_data,page:"my",info:"puber",qdata:qdata,name:req.session.name});
     });
   });
   }
- }
+}else{
+
+
+  if(req.query.info === "info"){
+    users.findOne({"nick":str},function(err,top_data){   //查找一个
+      res.render("info",{top_data:top_data,page:"other",info:"info",name:req.session.name});
+    });
+  }else  if(req.query.info === "focuslist"){
+  users.findOne({"nick":str},function(err,top_data){   //查找一个
+    res.render("info",{top_data:top_data,page:"other",info:"focuslist",name:req.session.name});
+  });
+}else if(req.query.info === "puber"){
+  users.findOne({"nick":str},function(err,top_data){
+  questions.find({puber:[req.query.user]},function(err,qdata){
+    console.log(qdata);
+    res.render("info",{top_data:top_data,page:"other",info:"puber",qdata:qdata,name:req.session.name});
+  });
+});
+}
+
+}
 });
 
 

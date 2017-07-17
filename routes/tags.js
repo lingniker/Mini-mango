999fa898dc0fae68535ae75ba615d5a4fca2b893
Xiaolong Lin <xiaolong145682@outlook.com>
2017-07-17 17:02:14 +0800

    xiugai tags.js by xiaolong
    commit by xiaolong
     要提交的变更：
    	修改：     routes/tags.js

--- routes/tags.js
+++ routes/tags.js
@@ -28,22 +28,32 @@ router.get(/^\/$/, function(req, res, next) {
         });
       };
       // 查询用户所关注的标签
-      users.find({nick:"shaoxu"},function(err,data1){
-        if(err){
-          console.log(err);
-        }else{
-          // console.log("标签数：",data1[0].tagfocuslist.length);
-          for(var j=0,len=(data1[0].tagfocuslist).length;j<len;j++){
-            tags.find({tag:(data1[0].tagfocuslist)[j]},function(err,data2){
-              if(err){
-                console.log(err);
-              }else{
-                asd2 = asd2.concat(data2);
+      if(req.session.name !== undefined){
+        users.find({nick:req.session.name},function(err,data1){
+          if(err){
+            console.log(err);
+          }else{
+            // console.log("标签数：",data1[0].tagfocuslist.length);
+            if(data1[0].tagfocuslist.length !== 0){
+              // console.log("======",data1[0].tagfocuslist);
+              for(var j=0,len=(data1[0].tagfocuslist).length;j<len;j++){
+                tags.find({tag:(data1[0].tagfocuslist)[j]},function(err,data2){
+                  if(err){
+                    console.log(err);
+                  }else{
+                    asd2 = asd2.concat(data2);
+                  }
+                });
               }
-            });
+            }else{
+               asd2 = [];
+            }
           }
-        }
-      });
+        });
+      }else{
+         asd2 = [];
+      }
+
       setTimeout(function(){
         // console.log("asd:",asd);
         if(asd.length !== 0){
@@ -52,12 +62,14 @@ router.get(/^\/$/, function(req, res, next) {
             nick:req.session.name // ======== ? 登录状态下的用户名
           },function(err,top_data){
             // console.log(data);
+            if(err) console.log(err);
             if(typeof(req.session.name) == 'undefined'){
               req.session.name = null;
             }
+            console.log("doc:",doc);
           return res.render("tags",{info:doc,biaoqian:asd,biaoqian2:asd2,top_data:top_data, name:req.session.name});
         });
-        }
+      }
       },10);
     }
   });

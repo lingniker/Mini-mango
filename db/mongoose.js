60f90a1b9399c1d397aba22063717d0d528df7ec
zhaohong <clarkkent.z@outlook.com>
2017-07-31 11:40:13 +0800

    click
    wenda

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
-var url = ("mongodb://mango:1@ds155132.mlab.com:55132/mini-mango");
-// var url = ("mongodb://127.0.0.1:27017/Mango");
+// var url = ("mongodb://mango:1@ds155132.mlab.com:55132/mini-mango");
+var url = ("mongodb://127.0.0.1:27017/Mango");
 mongoose.Promise = global.Promise;
 mongoose.connect(url);
 var db = mongoose.connection;
